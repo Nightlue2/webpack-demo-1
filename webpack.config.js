de0e6f1c4e5cf1bba57678a0d55616e6bdc02f94
Nightlue2 <basicperfect@outlook.com>
2020-07-28 10:41:41 +0800

    add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
+    devtool: 'inline-source-map',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -11,7 +12,18 @@ module.exports = {
     plugins: [new HtmlWebpackPlugin({
         title: '写代码啦',
         template: 'src/index.html'
-    })]
+    })],
+    devServer: {
+        contentBase: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ]
+    }
 };
 
 
