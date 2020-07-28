cef8f8e38edc38a3989b68df1010b5cfb682155c
Nightlue2 <basicperfect@outlook.com>
2020-07-28 10:14:34 +0800

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js'
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: '写代码啦',
+        template: 'src/index.html'
+    })]
 };
+
+
+
